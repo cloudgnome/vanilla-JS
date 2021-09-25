d11b23dee2c1ecef9d68c4cf9a56b02fd301e6c0
cloudgnome <icloudgnome@gmail.com>
2021-09-25 20:15:59 +0300

    Create mapdata.js

---
+++ mapdata.js
@@ -0,0 +1,14 @@
+var mapData = {
+	'P':'paragraph',
+	'H':'header',
+	'LI':'list',
+	'DIV':'paragraph',
+	'IMG':'image',
+	'undefined':'paragraph',
+	undefined:'paragraph',
+	'H2':'header',
+	'H3':'header',
+	'H4':'header',
+	'H5':'header',
+	'H6':'header',
+};
